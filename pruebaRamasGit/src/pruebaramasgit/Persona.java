d829400dc76cad52d18a7643a7958c017bc79b08
Fredy roman <johnfredyromanagudelo@gmail.com>
2022-07-22 21:23:28 -0500

    version inicial

---
+++ pruebaRamasGit/src/pruebaramasgit/Persona.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pruebaramasgit;
+
+/**
+ *
+ * @author Fredy Roman
+ */
+public class Persona {
+    
+    int id;
+    String nombre;
+    String primerApellido;
+    
+}
