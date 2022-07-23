a61a725f19c3fe927268426055c50006282b3ccd
Fredy roman <johnfredyromanagudelo@gmail.com>
2022-07-22 21:36:36 -0500

    version rama2

--- pruebaRamasGit/src/pruebaramasgit/Persona.java
+++ pruebaRamasGit/src/pruebaramasgit/Persona.java
@@ -15,5 +15,31 @@ public class Persona {
     int id;
     String nombre;
     String primerApellido;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPrimerApellido() {
+        return primerApellido;
+    }
+
+    public void setPrimerApellido(String primerApellido) {
+        this.primerApellido = primerApellido;
+    }
+    
+    
     
 }
